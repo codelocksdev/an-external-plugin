9e378c70c3df5dc2c1cee9ed2b5bcd6c0e4f6719
codelocksdev <codelocksdev@gmail.com>
2023-06-07 13:34:08 -0500

    init function entrypoint

--- apps/my-plugin/module-federation.config.js
+++ apps/my-plugin/module-federation.config.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'my-plugin',
   exposes: {
     './Module': './src/remote-entry.ts',
-    './pluginStart': './src/app/lifecycle/pluginStart.ts',
+    './init': './src/app/lifecycle/pluginStart.ts',
     './Tool': './src/app/tools/Tool.tsx',
   },
 };
